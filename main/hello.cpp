6a7e2b202f815eb3cb090276c169d60a060eb2eb
sahanabalajee <sahana.balajee@gmail.com>
2025-03-13 16:25:51 +0530

    Updated hello.cpp with an additional message

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, World!\n";
+  cout << "Hello, Jenkins!\n";
+  cout << "I have successfully built and run this.\n";
+  cout << "Now making changes and committing them!\n";
   return 0;
 }
